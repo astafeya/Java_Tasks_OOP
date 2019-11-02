7e95985dca300c4ef59370be8005cc75a0e8e899
Astafeva Irina <astafeya99@gmail.com>
2019-11-02 14:17:44 +0300

    create MyPoint and MyTriangle classes

---
+++ src/pointsAndTriangles/MyPoint.java
@@ -0,0 +1,62 @@
+package pointsAndTriangles;
+
+public class MyPoint {
+    private int x = 0;
+    private int y = 0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int[] getXY() {
+        int[] result = new int[2];
+        result[0] = x;
+        result[1] = y;
+        return result;
+    }
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public double distance(int x, int y) {
+        double answer = Math.sqrt(Math.pow((this.x - x), 2) + Math.pow((this.y - y), 2));
+        return answer;
+    }
+
+    public double distance(MyPoint another) {
+        double answer = Math.sqrt(Math.pow((this.x - another.x), 2) + Math.pow((this.y - another.y), 2));
+        return answer;
+    }
+
+    public double distance() {
+        double answer = Math.sqrt(Math.pow((this.x), 2) + Math.pow((this.y), 2));
+        return answer;
+    }
+}
