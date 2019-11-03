6bdfca74569475464deb550259614569100aa745
Astafeva Irina <astafeya99@gmail.com>
2019-11-04 01:32:09 +0300

    create Conteiner class

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import singleClasses.*;
+import authorsAndBooks.*;
+import pointsAndTriangles.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Ball ball = new Ball(50, 50, 5, 20, 45);
+        Container container = new Container(0, 100, 100, 100);
+
+        for (int i = 0; i < 5; i++) {
+            ball.move();
+            System.out.println(ball.getX() + " " + ball.getY());
+            System.out.println((container.collides(ball)) ? "Ball in container\n" : "Ball out of conteiner\n");
+        }
+    }
+}
---
+++ src/singleClasses/Container.java
@@ -0,0 +1,46 @@
+package singleClasses;
+
+public class Container {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public Container(int x, int y, int width, int height) {
+        this.x1 = x;
+        this.y1 = y;
+        this.x2 = width + x;
+        this.y2 = height - y;
+    }
+
+    public int getX() {
+        return x1;
+    }
+
+    public int getY() {
+        return y1;
+    }
+
+    public int getWidth() {
+        return x2 - x1;
+    }
+
+    public int getHeight() {
+        return y1 - y2;
+    }
+
+    public boolean collides(Ball ball) {
+        if ((ball.getX() + ball.getRadius() <= x2) &&
+                (ball.getX() - ball.getRadius() >= x1) &&
+                (ball.getY() + ball.getRadius() <= y1) &&
+                (ball.getY() - ball.getRadius() >= y2)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Container[(" + x1 + ", " + y1 + "),(" + x2 + ", " + y2 + ")]";
+    }
+}
